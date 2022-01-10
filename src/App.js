b695d17a758d09ccf4239e04d08c75399c3db3ae
dongjay00 <cjswodlehdgn@gmail.com>
2022-01-11 00:09:13 +0900

    wildcard 문자 * 활용하여 Not Found 페이지 연결

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from "./pages/About";
 import Article from "./pages/Article";
 import Articles from "./pages/Articles";
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 import Profile from "./pages/Profile";
 
 const App = () => {
@@ -17,6 +18,7 @@ const App = () => {
       <Route path="/articles" element={<Articles />}>
         <Route path=":id" element={<Article />} />
       </Route>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
