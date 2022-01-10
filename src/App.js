a2e088624bef4500f7062cd3db5dc9c0544817cf
dongjay00 <cjswodlehdgn@gmail.com>
2022-01-11 00:29:37 +0900

    React Suspense와 lazy import를 활용하여(코드 스플리팅), 현재 필요한 컴포넌트만 불러오고 다른 컴포넌트는 필요한 시점에 불러오도록 구현

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
+import * as React from "react";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
-import About from "./pages/About";
+// import About from "./pages/About";
 import Article from "./pages/Article";
-import Articles from "./pages/Articles";
+// import Articles from "./pages/Articles";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import MyPage from "./pages/MyPage";
 import NotFound from "./pages/NotFound";
-import Profile from "./pages/Profile";
+// import Profile from "./pages/Profile";
+
+const About = React.lazy(() => import("./pages/About"));
+const Profile = React.lazy(() => import("./pages/Profile"));
+const Articles = React.lazy(() => import("./pages/Articles"));
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/profiles/:username" element={<Profile />} />
+        <Route
+          path="/about"
+          element={
+            <React.Suspense fallback={<>...</>}>
+              <About />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="/profiles/:username"
+          element={
+            <React.Suspense fallback={<>...</>}>
+              <Profile />
+            </React.Suspense>
+          }
+        />
       </Route>
-      <Route path="/articles" element={<Articles />}>
+      <Route
+        path="/articles"
+        element={
+          <React.Suspense fallback={<>...</>}>
+            <Articles />
+          </React.Suspense>
+        }
+      >
         <Route path=":id" element={<Article />} />
       </Route>
       <Route path="/login" element={<Login />} />
