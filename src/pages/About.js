c7dc39a56d280833d91433835b531fa5c5667fb9
dongjay00 <cjswodlehdgn@gmail.com>
2022-01-10 23:34:22 +0900

    useLocation Hook을 활용하여 쿼리스트링 확인

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,17 @@
+import { useLocation } from "react-router-dom";
+
 const About = () => {
+  const location = useLocation();
+
   return (
     <div>
       <h1>소개</h1>
       <p>리액트 라우터를 사용해 보는 프로젝트입니다.</p>
+      <p>쿼리스트링: {location.search}</p>
+      <p>pathname: {location.pathname}</p>
+      <p>hash: {location.hash}</p>
+      <p>state: {location.state}</p>
+      <p>key: {location.key}</p>
     </div>
   );
 };
